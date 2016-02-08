40b6082871438c1394dfe652505f11aa1e3a5b7c
Kevin Perez <unknotted26@gmail.com>
2016-02-07 21:50:25 -0500

    first commit

---
+++ Grades.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Grades
+{
+public:
+  Grades();
+
+private:
+  
+  
+};
---
+++ GradesDriver.cpp
@@ -0,0 +1,16 @@
+/*
+  Kevin Perez
+  2/8/16
+  
+  This program calculates: max, min, median, Q1, Q2, mean, mode, and 
+  standard deviation of the provided values.
+
+*/
+#include <iostream>
+#include "Grades.h"
+
+int main(int argc, char * argv[])
+{
+  
+  return 0;
+}
